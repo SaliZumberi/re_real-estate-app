0852fdb0a96b3ae073795fd1cc5fe4a8ad69e630
Sali Zumberi <sali.zumberi@live.de>
2020-10-13 22:41:58 +0200

    import lazy module in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'buildings', pathMatch: 'full'},
+  {
+    path: 'buildings',
+    loadChildren: () => import('./buildings/buildings.module').then(m => m.BuildingsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
