071f5909fbd906f25afacb6b0262f3868330526f
Sali Zumberi <sali.zumberi@live.de>
2020-10-13 22:15:33 +0200

    create building component

--- src/app/buildings/buildings.module.ts
+++ src/app/buildings/buildings.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuildingsComponent } from './buildings.component';
+import { BuildingComponent } from './building/building.component';
 
 
 
 @NgModule({
-  declarations: [BuildingsComponent],
+  declarations: [BuildingsComponent, BuildingComponent],
   imports: [
     CommonModule
   ]
