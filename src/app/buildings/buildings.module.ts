52ad42201117a477295062bc68f57fcec51c2cae
Sali Zumberi <sali.zumberi@live.de>
2020-10-13 22:14:43 +0200

    create buildings module and import components

---
+++ src/app/buildings/buildings.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BuildingsComponent } from './buildings.component';
+
+
+
+@NgModule({
+  declarations: [BuildingsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BuildingsModule { }
