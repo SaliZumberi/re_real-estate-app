3ff2cdc1eb03882c6eab54cbad822e250ac45537
Sali Zumberi <sali.zumberi@live.de>
2020-10-16 18:18:23 +0200

    configure karam.conf file

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,10 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false,
+      jasmine: {
+        random: false,
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/real-estate-app'),
@@ -26,7 +29,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
+    singleRun: true,
+    restartOnFileChange: true,
+    browserConsoleLogOptions: {
+      level: 'log',
+      format: '%b %T: %m',
+      terminal: true
+    },
+  },);
 };
