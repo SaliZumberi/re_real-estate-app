6ec6c04cc11190bf34d897d28073c40e36ca7934
Sali Zumberi <sali.zumberi@live.de>
2020-10-16 12:34:15 +0200

    add mock quotes data for using them as random building descriptions
    better than lorem ipsum, right?

---
+++ src/app/shared/util/quotes.ts
@@ -0,0 +1,52 @@
+export const descriptions = [
+  {
+    "id": "1",
+    "quote": "The truly free man is the one who can turn down an invitation to dinner without giving an excuse.",
+    "author": "Jules Renard"
+  },
+  {
+    "id": "2",
+    "quote": "Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it's beauty.",
+    "author": "Albert Einstein"
+  },
+  {
+    "id": "3",
+    "quote": "She was free in her wildness. She was a wanderess, a drop of free water. She belonged to no man and to no city",
+    "author": " Roman Payne"
+  },
+  {
+    "id": "4",
+    "quote": "Maybe some women aren't meant to be tamed. Maybe they just need to run free until they find someone just as wild to run with them.",
+    "author": "Carrie Bradshaw"
+  },
+  {
+    "id": "5",
+    "quote": "Education is what remains after one has forgotten what one has learned in school.",
+    "author": "Albert Einstein "
+  },
+  {
+    "id": "6",
+    "quote": "Many books require no thought from those who read them, and for a very simple reason; they made no such demand upon those who wrote them",
+    "author": "Charles Caleb Colson"
+  },
+  {
+    "id": "7",
+    "quote": "Old age is fifteen years older than I am.",
+    "author": "Oliver Wendell Holmes"
+  },
+  {
+    "id": "8",
+    "quote": "Find the seed at the bottom of your heart and bring forth a flower.",
+    "author": "Shigenori Kameoka "
+  },
+  {
+    "id": "9",
+    "quote": "It may be true that the government that governs best governs least. Unfortunately, the same is also true of the government that governs worst.",
+    "author": "Jane Elizabeth Auer"
+  },
+  {
+    "id": "10",
+    "quote": "Literature is an occupation in which you have to keep proving your talent to people who have none.",
+    "author": "Jules Renard"
+  },
+]
