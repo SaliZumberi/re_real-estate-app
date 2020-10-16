286a5c719d647bfb7dfba510e55da89858922af7
Sali Zumberi <sali.zumberi@live.de>
2020-10-16 18:18:38 +0200

    move mockBuildings to data utils

--- src/app/shared/util/data.ts
+++ src/app/shared/util/data.ts
@@ -16,7 +16,37 @@ export const generateInitialBuildingEntity = (): Building => {
       createdAt: new Date(),
       updatedAt: new Date()
     },
-    description: descriptions[Math.floor(Math.random() * (descriptions.length - 0 + 1) + 0)].quote,
+    description: descriptions[Math.floor(Math.random() * descriptions.length)].quote,
     createdAt: new Date(),
     updatedAt: new Date()
-  };}
+  };
+}
+
+export const mockBuildings: Building[] = [
+  {
+    id: "1f4c7c7d-a988-4eef-92a7-e37f5980ee5c",
+    address: {
+      id: "0c49e0b6-13b6-47a0-88b0-bf5288424559",
+      streetName: "Mary Axe",
+      streetNumber: "30",
+      zip: 5201,
+      place: "London",
+      updatedAt: new Date(),
+      createdAt: new Date(),
+    },
+    imageUrl: "https://www.histerius.com/hs0719/swiss_re.jpg",
+    nickNames: [
+      {
+        id: "1f4c7c72d-a988-4eef-292a7-e237f5980ee5c",
+        name: "Test"
+      },
+      {
+        id: "1f4c7c7233d-a988-4eef-292a7-e237f5980ee5c",
+        name: "Baby"
+      }
+    ],
+    description: "Education is what remains after one has forgotten what one has learned in school.",
+    updatedAt: new Date(),
+    createdAt: new Date(),
+  }
+]
