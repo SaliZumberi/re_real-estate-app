aac215e30de53f66450061b2069a77cda9fdbd48
Sali Zumberi <sali.zumberi@live.de>
2020-10-16 12:25:52 +0200

    create nickname validator for unique nickname validations

---
+++ src/app/shared/modules/nickname-editor/validation.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {Observable, of, timer} from "rxjs";
+import {map, switchMap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+
+  constructor() {
+  }
+
+  nickNameValidator = (time = 400) => {
+    return (input: FormControl) => {
+      return timer(time).pipe(
+        switchMap(() => this.isValidNickname(input.value)),
+        map((isValidNickname: boolean) => {
+          console.log('{isValidNickname: isValidNickname}', isValidNickname)
+          return isValidNickname ? null : {nickNameExists: true}
+        })
+      );
+    };
+  };
+
+  isValidNickname(nickname: string): Observable<boolean> {
+    return of(nickname.charAt(0) == 'a')
+  }
+}
